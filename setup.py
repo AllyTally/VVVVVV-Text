be2a5f279ba1e6a983d9cd33105978e087a651be
AllyTally <mariomario13245@gmail.com>
2020-07-17 14:21:18 -0300

    bump ver num

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vvvvvv_text",
-    version="1.0.5",
+    version="1.0.6",
     author="Ally Tilde",
     author_email="alexiatilde@gmail.com",
     description="A GUI interface for creating VVVVVV textboxes",
